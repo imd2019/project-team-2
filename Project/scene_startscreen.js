f1d9a0d949cf38e8ba7e87dbeb7cdd4875e3421a
GrueFlo <florian.gruenewald@stud.h-da.de>
2020-06-14 17:30:49 +0200

    startbutton

--- Project/scene_startscreen.js
+++ Project/scene_startscreen.js
@@ -1,10 +1,15 @@
 import Scene from "./scene.js";
+import Button_Start from "./button_Start.js";
 
 export default class Startscreen extends Scene {
   constructor() {
     super("Startscreen");
+    this.startbutton;
   }
   init() {
+    this.startbutton=new Button_Start(1470,570);
+    this.startbutton.setRotInDegree(-18);
+    this.addChild(this.startbutton);
     this.addImage("background", window.ENUMS.IMAGE.BACKGROUND_STARTSCREEN);
     this.switchImage("background");
   }
