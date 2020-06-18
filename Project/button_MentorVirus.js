96a3a30a81893e931a153fd072bb1e560e0bd0e6
GrueFlo <florian.gruenewald@stud.h-da.de>
2020-06-18 16:03:56 +0200

    MentorVirus_VonAußenÄnderbar

--- Project/button_MentorVirus.js
+++ Project/button_MentorVirus.js
@@ -6,27 +6,16 @@ export default class Button_MentorVirus extends Button {
     super(x, y, 165, 165, window.ENUMS.SHAPE.RECT, "MentorVirus");
    // this.color = color(125, 125, 125);
    this.text=true;
-   this.mentorVirusText;
+   this.mentorVirusText = new Button_MentorVirusText (-150,70);
   }
 
   init() {
     this.addImage("MentorVirus", window.ENUMS.IMAGE.BUTTON_MENTORVIRUS);
-    
     this.switchImage("MentorVirus");
-    this.mentorVirusText = new Button_MentorVirusText (-150,70);
     this.addChild(this.mentorVirusText); 
     this.mentorVirusText.enable();
     console.log(this);
-    this.updateText();
-  }
 
-
-  released() {
-    console.log("released");
-  }
-
-  pressed() {
-    console.log("pressed");
   }
 
   updateText(string) {
