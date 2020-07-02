fecb26894659f771f73e14ad043ad6320868fcfe
diandrahermann <diandrahermann@users.noreply.github.com>
2020-07-03 00:22:21 +0200

    Doku und Über uns html

--- Project/main.js
+++ Project/main.js
@@ -52,7 +52,7 @@ function draw() {
     testScene2.addChild(test);
     world.onInit();
 
-    world.nextScene(window.ENUMS.SCENE_NAMES.START);
+    world.nextScene(window.ENUMS.SCENE_NAMES.PEOPLE_BOUNCY);
 
     init = false;
   }
